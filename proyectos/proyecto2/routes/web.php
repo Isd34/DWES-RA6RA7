5219b2a0bb2dd66ca26b3e4394abdf2182567afa
DiegoEspMig <diegoesperanzamiguel@gmail.com>
2026-02-02 17:41:30 +0100

    Cambio de declaración de rutas api (routes/web.php -> bootstrap/app.php) para Laravel 11 o superior

--- proyectos/proyecto2/routes/web.php
+++ proyectos/proyecto2/routes/web.php
@@ -2,11 +2,3 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Estas rutas son de la API, y no de la web, y se comportan
-// como API
-// Cargar rutas API con middleware (guardián de seguridad)
-// api para evitar CSRF
-// group = grupo, están todas en grupo en un archivo
-Route::middleware('api')->group(function () {
-    require __DIR__ . '/api.php';
-});
